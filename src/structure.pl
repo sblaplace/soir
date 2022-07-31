0b1ce00b56519373d1f2b5a866a9183714c39caf
Cactus <sblaplace@gmail.com>
2022-07-31 10:22:23 +0200

    Add instr structure

--- src/structure.pl
+++ src/structure.pl
@@ -2,6 +2,7 @@
 :- use_module(library(lists)).
 :- use_module(library(charsio)).
 :- use_module(library(clpz)).
+:- use_module(library(dcgs)).
 
 vec(A) :- length(A, L), L #< 2^32.
 
@@ -29,10 +30,10 @@ name(Name) :-
 
 % Types
 
-valtype(i(32, N)).
-valtype(i(64, N)).
-valtype(i(32, N)).
-valtype(i(64, N)).
+valtype(i(32, _N)).
+valtype(i(64, _N)).
+valtype(i(32, _N)).
+valtype(i(64, _N)).
 
 resulttype([]).
 resulttype([valtype(_) | resulttype]).
@@ -62,6 +63,95 @@ externtype(global(globaltype(_))).
 
 nn(32).
 nn(64).
+mm(32).
+mm(64).
 sx(u).
 sx(s).
-instr()
+instr(inn_const(nn(N), i(nn(N), _))).
+instr(fnn_const(nn(N), f(nn(N), _))).
+instr(inn_iunop(nn(_), iunop(_))).
+instr(fnn_funop(nn(_), funop(_))).
+instr(inn_ibinop(nn(_), ibinop(_))).
+instr(fnn_fbinop(nn(_), fbinop(_))).
+instr(inn_itestop(nn(_), itestop(_))).
+instr(inn_irelop(nn(_), irelop(_))).
+instr(fnn_frelop(nn(_), frelop(_))).
+instr(i32_wrap_i64).
+instr(i64_extend_i32_sx(sx(_))).
+instr(inn_trunc_fmm_sx(nn(_), mm(_), sx(_))).
+instr(f32_demote_f64).
+instr(f64_promote_f32).
+instr(fnn_convert_imm_sx(nn(_), mm(_), sx(_))).
+instr(inn_reinterpret_fnn(nn(_))).
+instr(fnn_reinterpret_inn(nn(_))).
+iunop(clz).
+iunop(ctz).
+iunop(popcnt).
+ibinop(add).
+ibinop(sub).
+ibinop(mul).
+ibinop(div_u).
+ibinop(div_s).
+ibinop(rem_u).
+ibinop(rem_s).
+ibinop(and).
+ibinop(or).
+ibinop(xor).
+ibinop(shl).
+ibinop(shr_u).
+ibinop(shr_s).
+ibinop(rotl).
+ibinop(rotr).
+funop(abs).
+funop(neg).
+funop(sqrt).
+funop(ceil).
+funop(floor).
+funop(trunc).
+funop(nearest).
+fbinop(add).
+fbinop(sub).
+fbinop(mul).
+fbinop(div).
+fbinop(min).
+fbinop(max).
+fbinop(copysign).
+itestop(eqz).
+irelop(eq).
+irelop(ne).
+irelop(lt_u).
+irelop(lt_s).
+irelop(le_u).
+irelop(le_s).
+irelop(ge_u).
+irelop(ge_s).
+frelop(eq).
+frelop(ne).
+frelop(lt).
+frelop(gt).
+frelop(le).
+frelop(ge).
+
+unop(iunop(_)).
+unop(funup(_)).
+binop(ibinop(_)).
+binop(fbinop(_)).
+testop(itestop(_)).
+relop(irelop(_)).
+relop(frelop(_)).
+cvtop(wrap).
+cvtop(extend).
+cvtop(trunc).
+cvtop(convert).
+cvtop(demote).
+cvtop(promote).
+cvtop(reinterpret).
+
+instr(drop(_)).
+instr(select(_)).
+
+instr(local_get(localidx(_))).
+instr(local_set(localidx(_))).
+instr(local_tee(localidx(_))).
+instr(global_get(globalidx(_))).
+instr(global_set(globalidx(_))).
